a2b349f361d9d2514f4c16934ce1864d65e34cc7
rahnik <rahnik14@gmail.com>
2020-09-03 11:38:35 +0300

    yeet

---
+++ models/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'dbconnect.php';
+
+class Book {
+
+    public function findById ( $id ) {
+
+        global $pdo;
+
+        $stmt = $pdo->prepare('SELECT * FROM books WHERE id=:id');
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Book');
+        $stmt->execute(['id' => $id]);
+
+        return $stmt->fetch();
+
+    }
+}
